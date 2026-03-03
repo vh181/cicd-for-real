6e46042efb4741b883fc7e05409355bfae0d054d
Victor Hugo <dinizvictorhugo18@gmail.com>
2026-03-03 12:40:25 -0300

    primeiro codigo

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Sistema PetPay iniciado com sucesso!");
+console.log("Ambiente de CI/CD configurado.");
+
+function soma(a, b) {
+    return a + b;
+}
+
+console.log("Teste de soma (2+3):", soma(2, 3));
