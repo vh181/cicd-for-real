ea54e8f4cff0e058146e37054e3ab5e09d605825
Victor Hugo <dinizvictorhugo18@gmail.com>
2026-03-03 13:20:11 -0300

    atualizando para versão 3.0.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('PetPay - Sistema Online via CD!');
+  res.end('PetPay - Versão 2.0 no ar!');
 });
 
 server.listen(port, () => {
